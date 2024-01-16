4b06ac78a6df6cf821faf235506579aa34f356cc
KanisornNualsri <kanisorn.nua@furukawaelectric.com>
2024-01-16 13:36:38 +0700

    Add project files.

---
+++ test_edge/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "start" Then
+            Button1.Text = "stop"
+        Else
+            Button1.Text = "start"
+        End If
+    End Sub
+End Class
